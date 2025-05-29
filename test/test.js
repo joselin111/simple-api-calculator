fd0be4feffd591dac7a4d371bf0791b54cb45201
joselin111 <joselinj135@gmail.com>
2025-05-28 21:07:18 -0400

    test.js

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Sample Test', () => {
+  it('should pass this simple test', () => {
+    expect(5 + 3).to.equal(8);
+  });
+});
